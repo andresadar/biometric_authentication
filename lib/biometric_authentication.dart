88877e918339717cafbeb1603024386893f18740
Andres <desarrollador.its10@metgroupsas.com>
2023-04-27 16:49:21 -0500

    add local_auth

--- lib/biometric_authentication.dart
+++ lib/biometric_authentication.dart
@@ -1,6 +1,5 @@
 library biometric_authentication;
 
-
 export 'package:biometric_authentication/biometric/biometric.dart';
-export  'package:local_auth/local_auth.dart' show BiometricType;
+export  'package:local_auth/local_auth.dart';
 
